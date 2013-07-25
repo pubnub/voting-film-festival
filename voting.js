238c2faa49c6f9f296ff489db16dfd6c40eba24d
Stephen Blum <blum.stephen@gmail.com>
2013-07-24 21:24:49 -0700

    updated voting with improved signle vote counting mechanic.

--- voting.js
+++ voting.js
@@ -7,6 +7,7 @@
 var vote_settings       = { publish_key : 'demo', subscribe_key : 'demo' }
 ,   vote_channel        = PUBNUB.$('film-vote-channel').innerHTML
 ,   pubnub              = PUBNUB.init(vote_settings)
+,   my_uuid             = get_my_uuid()
 ,   film_display        = PUBNUB.$('film-display')
 ,   film_template       = PUBNUB.$('film-template').innerHTML
 ,   film_data           = PUBNUB.$('film-data').innerHTML
@@ -30,7 +31,35 @@ film_display.innerHTML = film_display_buffer.join('');
 delegate( film_display, 'clicks' );
 
 PUBNUB.events.bind( 'clicks.vote', function(data) {
-    pubnub.publish({ channel : vote_channel, message : data });
+    var button  = data.target.getElementsByTagName('a')[0]
+    ,   filmbox = data.target.parentNode
+    ,   disable = 'btn btn-large btn-block btn-disable';
+
+    // Prevent Douplicate Votes
+    if (pubnub.attr( button, 'data-voted' )) return;
+    pubnub.attr( button, 'data-voted', 'true' );
+
+    // Disable Button Interface
+    PUBNUB.attr( button, 'class', disable );
+    button.className = disable;
+
+    // Disable Film Box
+    PUBNUB.css( filmbox, {
+        opacity    : 0.2,
+        background : "#87f496",
+        color      : "#fff"
+    } );
+
+    // Debug
+    console.log( data );
+
+    // Deliver Vote
+    pubnub.publish({ channel : vote_channel, message : {
+        uuid   : my_uuid,
+        film   : data.data,
+        action : data.action
+    } });
+    
 } );
 
 
@@ -48,18 +77,28 @@ function parse_film_file( data, format ) {
     });
 }
 
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+/* GET MY UUID
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+function get_my_uuid() {
+    var my_uuid = PUBNUB.db.get('my-uuid') || PUBNUB.uuid();
+    PUBNUB.db.set( 'my-uuid', my_uuid );
+    return my_uuid;
+}
+
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // GET ELEMENT ACTION DATA ATTRIBUTE AND FIRE ASSOCIATED EVENT
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 function delegate( element, namespace ) {
-    PUBNUB.bind( 'mousedown,touchstart', element, function(e) {
+    PUBNUB.bind( 'click', element, function(e) {
         var data   = bubblefind( e, 'data-data' )
         ,   action = bubblefind( e, 'data-action' );
         if (!action) return true;
-        PUBNUB.events.fire( namespace + '.' + action, {
-            action : action,
-            data   : data
+        PUBNUB.events.fire( namespace + '.' + action.result, {
+            action : action.result,
+            target : action.target,
+            data   : data.result
         } );
     } );
 }
@@ -69,7 +108,7 @@ function bubblefind( e, attr ) {
     ,   result = '';
     while (target) {
         result = PUBNUB.attr( target, attr );
-        if (result) return result;
+        if (result) return { result : result, target : target };
         target = target.parentNode;
     }
 }
